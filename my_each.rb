7a7b38b4beb492424ec172ff698c3f7815e25794
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-31 02:03:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,24 @@
-def my_each # put argument(s) here
-  # code here
+list = ["cam", "bam", "sam"]
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 end
+
+my_each(list) {|i| puts "What is #{i} doing today?"}
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| puts "Hey #{i}, how are you?" }
